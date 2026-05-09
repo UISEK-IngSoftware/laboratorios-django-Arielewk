b4f963250b97dec7cb1152ca37c0299c663067c2
Ariel Velásquez <rommel.velasquez@uisek.edu.ec>
2026-05-09 18:51:00 -0500

    Laboratorio 5 finalizado

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,12 +1,35 @@
 from django.db import models
 
+
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    level = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Pokemon(models.Model):
-    name = models.CharField(max_length=100, null=False)
-    type = models.CharField(max_length=50, null=False)
-    height = models.FloatField()
-    weight = models.FloatField()
-    
-    
+    name = models.CharField(max_length=100)
+
+    POKEMON_TYPES = [
+        ('A', 'Agua'),
+        ('F', 'Fuego'),
+        ('T', 'Tierra'),
+        ('P', 'Planta'),
+        ('E', 'Eléctrico'),
+        ('L', 'Lagartija'),
+    ]
+
+    type = models.CharField(max_length=1, choices=POKEMON_TYPES, null=False)
+    height = models.DecimalField(decimal_places=4, max_digits=6)
+    weight = models.DecimalField(decimal_places=4, max_digits=6)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null= True)
+    picture = models.ImageField(upload_to="pokemon_images", null=True, blank=True)
+
+ 
+
     def __str__(self):
         return self.name
-    
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,16 +1,30 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 def index(request):
     pokemons = Pokemon.objects.all() #select * from pokedex_pokemon
+    trainers = Trainer.objects.all() 
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({
+        'pokemons': pokemons,
+        'trainers': trainers
+        }, 
+        request))
 
 def pokemon(request, id:int):
     pokemon = Pokemon.objects.get(id=id) #select * from pokedex_pokemon where id = id
     template = loader.get_template('display_pokemon.html')
     context = {
         'pokemon': pokemon
+    }
+    return HttpResponse(template.render(context, request)) 
+
+
+def trainer_details(request, id:int):
+    trainer = Trainer.objects.get(id=id) #select * from pokedex_pokemon where id = id
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
     }
     return HttpResponse(template.render(context, request)) 
